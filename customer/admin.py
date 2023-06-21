35f1acd1894496ac2e6eb9b177c5142d60656feb
nkathamilcahmakini <nkathamilcah@gmail.com>
2023-06-21 19:09:17 +0300

    Created an app Customer that has the attributes of a customer in a mama mboga app

--- customer/admin.py
+++ customer/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
+from .models import Customer
 # Register your models here.
 
 
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'purchase_product', 'price', 'quantity', 'total_price', 'mode_of_payment')
+admin.site.register(Customer, CustomerAdmin)    
--- customer/models.py
+++ customer/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    purchase_product = models.CharField(max_length=255)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    total_price = models.FloatField()
+    mode_of_payment = models.CharField(max_length=255)
 
