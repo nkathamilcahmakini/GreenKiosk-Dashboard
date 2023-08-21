f87d2f6e47f484a1c0ce63e11e85e046a56e82ed
nkathamilcahmakini <nkathamilcah@gmail.com>
2023-08-21 18:14:44 +0300

    Added Templates

--- payment/views.py
+++ payment/views.py
@@ -1,28 +1,12 @@
-from django.shortcuts import render ,redirect
+from django.shortcuts import render
 from .forms import PaymentUploadForm
-from .models import Payment
 
-def payment_upload_view(request):
-    form = PaymentUploadForm()
-    return render(request,"payment/payment_upload.html", {"form":form})
-
-def payments_list_view(request):
-    payments = Payment.objects.all()
-    return render(request,"payment/payment_list.html",{"payment": payments})
-
-
-def payment_detail(request,id):
-    payment = Payment.objects.get(id=id)
-    return render(request,"payment/payment_detail.html", {"payment":payment})
-
-def payment_update_view(request, id):
-    payment = Payment.objects.get(id=id)
+def create_payment(request):
     if request.method == 'POST':
-        form = PaymentUploadForm(request.POST, instance=payment)
+        form = PaymentUploadForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("payment_detail_view", id=payment.id)
-
     else:
-        form =PaymentUploadForm(instance=payment)
-        return render(request, "payment/edit_payment.html", {'form': form})
+        form = PaymentUploadForm()
+    
+    return render(request, 'payment/create_payment.html', {'form': form})
